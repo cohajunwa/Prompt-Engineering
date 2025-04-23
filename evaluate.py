128cc915f401d0712c380528e837ef94cede4305
Comfort Ohajunwa <comfortohajunwa1@gmail.com>
2025-04-23 02:17:45 -0400

    Slight change to cosine similarity function to output float (rather than numpy float)

--- evaluate.py
+++ evaluate.py
@@ -15,7 +15,7 @@ def _get_codebert_model():
 
 def get_cosine_similarity(response1, response2):
     embeddings = _get_codebert_model().encode([response1, response2])
-    return cosine_similarity([embeddings[0]], [embeddings[1]])[0][0]
+    return float(cosine_similarity([embeddings[0]], [embeddings[1]])[0][0])
 
 def get_bleu_4_score(response_1, response_2):
   return sacrebleu.corpus_bleu([response_1], [[response_2]]).score
